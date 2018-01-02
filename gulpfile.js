af6b5711fd7b7945d551f4c1396e68f4eae6db23
shinate <shine.wangrs@gmail.com>
2018-01-02 21:07:57 +0800

    调整打包

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,43 @@ var gulp = require('gulp');
 var gulpZip = require('gulp-zip');
 var gulpUnzip = require('gulp-unzip');
 var spawn = require('cross-spawn');
+var sf = require('split-file');
+var fs = require('fs');
+var path = require('path');
 
-gulp.task('zip', function () {
+gulp.task('zip', ['zip:pack'], function () {
+    return sf.splitFileBySize('src/design.zip', 8000000)
+        .then(function (names) {
+            console.log(names);
+        })
+        .catch(function (err) {
+            console.log('Error: ', err);
+        });
+});
+
+gulp.task('zip:pack', function () {
     return gulp.src('src/design/**/*')
         .pipe(gulpZip('design.zip'))
         .pipe(gulp.dest('src'));
 });
 
-gulp.task('unzip', function () {
+gulp.task('zip:merge', function () {
+    var parts = fs.readdirSync('src').map(function (file) {
+        return 'src/' + file;
+    }).filter(function (file) {
+        return fs.statSync(file).isFile() && -1 < path.extname(file).indexOf('.sf');
+    });
+
+    return sf.mergeFiles(parts, 'src/design.zip')
+        .then(function (names) {
+            console.log(names);
+        })
+        .catch(function (err) {
+            console.log('Error: ', err);
+        });
+});
+
+gulp.task('unzip', ['zip:merge'], function () {
     return gulp.src('src/design.zip')
         .pipe(gulpUnzip())
         .pipe(gulp.dest('src/design'));
